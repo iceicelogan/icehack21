1d5bcb692bfc72937a6b575da7ab97583428db48
Logan <clarkloganice@gmail.com>
2021-10-02 13:59:54 -0500

    Adding Text Analytics
    Adding our first rounds of text analytics, specifically creating a cleaned corpus to prepare a DTM

--- bookscrape.R
+++ bookscrape.R
@@ -9,6 +9,9 @@ library(ngram)
 
 library(tidyverse)
 
+install.packages('corpus')
+library(corpus)
+
 # Read the Book as text
 url <- 'https://www.gutenberg.org/files/2600/2600-h/2600-h.htm'
 webpage <- read_html(url)
@@ -25,3 +28,18 @@ book_newest <- gsub("[\r\n]", "", book_newer)
 
 #check it
 head(book_newest, 5)
+
+# Create Corpus
+book_corpus <- as_corpus_text(book_newest)
+
+# Test ngram
+install.packages('tm')
+install.packages('tau')
+library(tm)
+library(tau)
+
+tokenize_ngrams <- function(x, n=3) return(rownames(as.data.frame(unclass(textcnt(x,method="string",n=n)))))
+
+texts <- c("This is the first document.", "This is the second file.", "This is the third text.")
+corpus <- Corpus(VectorSource(texts))
+matrix <- DocumentTermMatrix(corpus,control=list(tokenize=tokenize_ngrams))
