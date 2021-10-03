8ba2b3ffcc0e26ba0f01531c61df393f034807ef
Logan <clarkloganice@gmail.com>
2021-10-02 20:40:15 -0500

    Finalized ngrams
    cleaned up and finalized 1-4 grams

--- bookscrape.R
+++ bookscrape.R
@@ -6,12 +6,11 @@ install.packages('xml2')
 library(xml2)
 install.packages('ngram')
 library(ngram)
-
 library(tidyverse)
-
 install.packages('corpus')
 library(corpus)
-
+install.packages('tidytext')
+library(tidytext)
 # Read the Book as text
 url <- 'https://www.gutenberg.org/files/2600/2600-h/2600-h.htm'
 webpage <- read_html(url)
@@ -25,66 +24,42 @@ book <- html_text(book_html)
 book_new <- gsub('\r\n      ', ' ', book)
 book_newer <- gsub('\r\n    ', ' ', book_new)
 book_newest <- gsub("[\r\n]", "", book_newer)
-
+b2 <- as.String(book_newest)
 #check it
-head(book_newest, 5)
+b3 <- gsub('[[:punct:] ]+',' ', b2)
+b4 <- as.data.frame(book)
 
-# Create Corpus
-book_corpus <- as_corpus_text(book_newest)
+b5 <- data.frame(txt = b4$book,
+           stringsAsFactors = FALSE)
 
-# Test ngram
-install.packages('tm')
-install.packages('tau')
-library(tm)
-library(tau)
-
-tokenize_ngrams <- function(x, n=3) return(
-  rownames(
-    as.data.frame(
-      unclass(
-        textcnt(
-          x,method="string",n=n)))))
-
-#texts <- c("This is the first document.", "This is the second file.", "This is the third text.")
-corpus <- Corpus(VectorSource(book_newest))
-matrix <- DocumentTermMatrix(corpus,control=list(tokenize=tokenize_ngrams))
+bigrams <- b5 %>% 
+  unnest_tokens(word, txt, token = "ngrams", n = 2) %>% 
+  separate(word, c("word1", "word2"), sep = " ") %>% 
+  filter(!word1 %in% stop_words$word) %>%
+  filter(!word2 %in% stop_words$word) %>% 
+  unite(word,word1, word2, sep = " ") %>% 
+  count(word, sort = TRUE) %>%
+  filter(word != "NA NA")
 
 
-#TextDoc_dtm <- TermDocumentMatrix(book_newest)
-#dtm_m <- as.matrix(TextDoc_dtm)
-# Sort by descearing value of frequency
-#dtm_v <- sort(rowSums(dtm_m),decreasing=TRUE)
-#dtm_d <- data.frame(word = names(dtm_v),freq=dtm_v)
-# Display the top 5 most frequent words
-#head(dtm_d, 5)
+trigrams <- b5 %>% 
+  unnest_tokens(word, txt, token = "ngrams", n = 3) %>% 
+  separate(word, c("word1", "word2", "word3"), sep = " ") %>% 
+  filter(!word1 %in% stop_words$word) %>%
+  filter(!word2 %in% stop_words$word) %>% 
+  filter(!word3 %in% stop_words$word) %>% 
+  unite(word,word1, word2, word3, sep = " ") %>% 
+  count(word, sort = TRUE) %>%
+  filter(word != "NA NA NA")
 
+fourgrams <- b5 %>% 
+  unnest_tokens(word, txt, token = "ngrams", n = 4) %>% 
+  separate(word, c("word1", "word2", "word3", "word4"), sep = " ") %>% 
+  filter(!word1 %in% stop_words$word) %>%
+  filter(!word2 %in% stop_words$word) %>% 
+  filter(!word3 %in% stop_words$word) %>% 
+  filter(!word4 %in% stop_words$word) %>% 
+  unite(word,word1, word2, word3, word4, sep = " ") %>% 
+  count(word, sort = TRUE) %>%
+  filter(word != "NA NA NA NA")
 
-# Load the data as a corpus
-TextDoc <- Corpus(VectorSource(book_newest))
-
-#Replacing "/", "@" and "|" with space
-toSpace <- content_transformer(function (x , pattern ) gsub(pattern, " ", x))
-TextDoc <- tm_map(TextDoc, toSpace, "/")
-TextDoc <- tm_map(TextDoc, toSpace, "@")
-TextDoc <- tm_map(TextDoc, toSpace, "\\|")
-# Convert the text to lower case
-TextDoc <- tm_map(TextDoc, content_transformer(tolower))
-# Remove numbers
-TextDoc <- tm_map(TextDoc, removeNumbers)
-# Remove english common stopwords
-TextDoc <- tm_map(TextDoc, removeWords, stopwords("english"))
-# Remove punctuations
-TextDoc <- tm_map(TextDoc, removePunctuation)
-# Eliminate extra white spaces
-TextDoc <- tm_map(TextDoc, stripWhitespace)
-# Text stemming - which reduces words to their root form
-TextDoc <- tm_map(TextDoc, stemDocument)
-
-# Build a term-document matrix
-TextDoc_dtm <- TermDocumentMatrix(TextDoc)
-dtm_m <- as.matrix(TextDoc_dtm)
-# Sort by descearing value of frequency
-dtm_v <- sort(rowSums(dtm_m),decreasing=TRUE)
-dtm_d <- data.frame(word = names(dtm_v),freq=dtm_v)
-# Display the top 5 most frequent words
-head(dtm_d, 5)
